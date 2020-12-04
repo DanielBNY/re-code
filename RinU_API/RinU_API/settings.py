43238eb157d6f5ff24496e4b3c57a768e929639a
Daniel <daniel.benyaacov@gmail.com>
2020-12-05 00:55:45 +0200

    change default db name to RinU

--- RinU_API/RinU_API/settings.py
+++ RinU_API/RinU_API/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ExtractorsLibs.apps.ExtractorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'RinU_API.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.RinU',
     }
 }
 
