b03bcfd9f0e3e062467af98f5a630a2695f3c718
Daniel <daniel.benyaacov@gmail.com>
2020-12-05 01:00:41 +0200

    Fix name to correct folder name

--- RinU_API/RinU_API/settings.py
+++ RinU_API/RinU_API/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ExtractorsLibs.apps.ExtractorsConfig',
+    'Extractors.apps.ExtractorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
