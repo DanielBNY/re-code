45f07212f0e071347515865522559995da4c6025
Daniel <daniel.benyaacov@gmail.com>
2020-12-12 14:10:55 +0200

    Change id to by name:number, remove size

--- RinuAPI/Extractors/models.py
+++ RinuAPI/Extractors/models.py
@@ -1,31 +1,27 @@
 class FunctionInfo:
-    def __init__(self, size, address):
+    def __init__(self, address):
         """
         id: string
-        size: string
         file_id: string (id to hashes)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
-        self.id = address
-        self.size = size
+        self.id = f"function:{address}"
         self.file_id = f"file:{address}"
         self.calls_out_set_id = f"function:{address}:calls_out"
         self.calls_in_set_id = f"function:{address}:calls_in"
 
 
 class FileInfo:
-    def __init__(self, size, file_id):
+    def __init__(self, file_id):
         """
         id: string
-        size: string
         folder_id: string (id to hashes)
         contained_functions: string (set id)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
-        self.id = file_id
-        self.size = size
+        self.id = f"file:{file_id}"
         self.folder_id = f"folder:{file_id}"
         self.contained_functions_set_id = f"file:{file_id}:contained_functions"
         self.calls_out_set_id = f"file:{file_id}:calls_out"
@@ -33,16 +29,14 @@ class FileInfo:
 
 
 class FolderInfo:
-    def __init__(self, size, folder_id):
+    def __init__(self, folder_id):
         """
         id: string
-        size: string
         contained_files: string (id to hashes)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
-        self.id = folder_id
-        self.size = size
+        self.id = f"folder:{folder_id}"
         self.contained_files_set_id = f"folder:{folder_id}:contained_files"
         self.calls_out_set_id = f"folder:{folder_id}:calls_out"
         self.calls_in_set_id = f"folder:{folder_id}:calls_in"
