3e65993c106c4ddb00131fea469676baf0c03733
Daniel <daniel.benyaacov@gmail.com>
2020-12-12 13:40:09 +0200

    Add id to the models

--- RinuAPI/Extractors/models.py
+++ RinuAPI/Extractors/models.py
@@ -1,11 +1,13 @@
 class FunctionInfo:
     def __init__(self, size, address):
         """
+        id: string
         size: string
         file_id: string (id to hashes)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
+        self.id = address
         self.size = size
         self.file_id = f"file:{address}"
         self.calls_out_set_id = f"function:{address}:calls_out"
@@ -15,12 +17,14 @@ class FunctionInfo:
 class FileInfo:
     def __init__(self, size, file_id):
         """
+        id: string
         size: string
         folder_id: string (id to hashes)
         contained_functions: string (set id)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
+        self.id = file_id
         self.size = size
         self.folder_id = f"folder:{file_id}"
         self.contained_functions_set_id = f"file:{file_id}:contained_functions"
@@ -31,11 +35,13 @@ class FileInfo:
 class FolderInfo:
     def __init__(self, size, folder_id):
         """
+        id: string
         size: string
         contained_files: string (id to hashes)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
+        self.id = folder_id
         self.size = size
         self.contained_files_set_id = f"folder:{folder_id}:contained_files"
         self.calls_out_set_id = f"folder:{folder_id}:calls_out"
