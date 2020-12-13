70c820d51e205d6038dd71f75bd91408b8b03791
Daniel <daniel.benyaacov@gmail.com>
2020-12-13 19:08:42 +0200

    Add a contained address field for the model

--- RinuAPI/Extractors/models.py
+++ RinuAPI/Extractors/models.py
@@ -1,42 +1,48 @@
 class FunctionInfo:
-    def __init__(self, address):
+    def __init__(self, contained_address):
         """
         id: string
+        contained_address: string
         file_id: string (id to hashes)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
-        self.id = f"function:{address}"
-        self.file_id = f"file:{address}"
-        self.calls_out_set_id = f"function:{address}:calls_out"
-        self.calls_in_set_id = f"function:{address}:calls_in"
+        self.id = f"function:{contained_address}"
+        self.contained_address = contained_address
+        self.file_id = f"file:{contained_address}"
+        self.calls_out_set_id = f"function:{contained_address}:calls_out"
+        self.calls_in_set_id = f"function:{contained_address}:calls_in"
 
 
 class FileInfo:
-    def __init__(self, file_id):
+    def __init__(self, contained_address):
         """
         id: string
+        contained_address: string
         folder_id: string (id to hashes)
         contained_functions: string (set id)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
-        self.id = f"file:{file_id}"
-        self.folder_id = f"folder:{file_id}"
-        self.contained_functions_set_id = f"file:{file_id}:contained_functions"
-        self.calls_out_set_id = f"file:{file_id}:calls_out"
-        self.calls_in_set_id = f"file:{file_id}:calls_in"
+        self.id = f"file:{contained_address}"
+        self.contained_address = contained_address
+        self.folder_id = f"folder:{contained_address}"
+        self.contained_functions_set_id = f"file:{contained_address}:contained_functions"
+        self.calls_out_set_id = f"file:{contained_address}:calls_out"
+        self.calls_in_set_id = f"file:{contained_address}:calls_in"
 
 
 class FolderInfo:
-    def __init__(self, folder_id):
+    def __init__(self, contained_address):
         """
         id: string
+        contained_address: string
         contained_files: string (id to hashes)
         calls_out_set_id: string (set id)
         calls_in_set_id: string (set id)
         """
-        self.id = f"folder:{folder_id}"
-        self.contained_files_set_id = f"folder:{folder_id}:contained_files"
-        self.calls_out_set_id = f"folder:{folder_id}:calls_out"
-        self.calls_in_set_id = f"folder:{folder_id}:calls_in"
+        self.id = f"folder:{contained_address}"
+        self.contained_address = contained_address
+        self.contained_files_set_id = f"folder:{contained_address}:contained_files"
+        self.calls_out_set_id = f"folder:{contained_address}:calls_out"
+        self.calls_in_set_id = f"folder:{contained_address}:calls_in"
