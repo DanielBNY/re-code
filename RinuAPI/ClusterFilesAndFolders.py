57cf2ae9580177bbde9171296510f1cd8b1e644b
Daniel <daniel.benyaacov@gmail.com>
2020-12-17 18:56:37 +0200

    The function could be used for functions and folders

--- RinuAPI/ClusterFilesAndFolders.py
+++ RinuAPI/ClusterFilesAndFolders.py
@@ -2,8 +2,8 @@ class ClusterFilesAndFolders:
     def __init__(self, redis_session):
         self.redis_session = redis_session
 
-    def get_average_function_size(self):
-        functions_id = self.redis_session.smembers('functions')
+    def get_average_node_size(self, functions_or_folders):
+        functions_id = self.redis_session.smembers(functions_or_folders)
         number_of_functions = len(functions_id)
         functions_size_sum = 0
         for function_id in functions_id:
