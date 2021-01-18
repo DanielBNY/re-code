401f97b8d4a70bdddaac89bef828eeaeedb45e17
Daniel <daniel.benyaacov@gmail.com>
2021-01-18 17:09:37 +0200

    Add a logic which connect two trees heads when nodes from the two trees are connected

--- HierarchyExtractor/CallTreeExtractor.py
+++ HierarchyExtractor/CallTreeExtractor.py
@@ -58,10 +58,44 @@ class CallTreeExtractor:
         for called_function_model in functions_calls_out_models:
             called_file_model = called_function_model.get_parent_file_model()
             file_calls_in_models = called_file_model.get_call_in_files_models()
+            origin_file_repo = FileModel(contained_address=origin_function_model.contained_address,
+                                         redis_session=self.redis_session)
             if not bool(file_calls_in_models):
-                origin_file_repo = FileModel(contained_address=origin_function_model.contained_address,
-                                             redis_session=self.redis_session)
                 origin_file_repo.recursion_add_edge(called_file_model.contained_address)
                 neighbors_to_revisit.append(FunctionModel(function_id=called_function_model.model_id,
                                                           redis_session=self.redis_session))
+            else:
+                self.connect_two_trees(origin_file_repo, called_file_model)
+
         return neighbors_to_revisit
+
+    def connect_two_trees(self, first_tree_file: FileModel, second_tree_file: FileModel):
+        first_head_and_relative_distance = self.get_head_and_relative_distance(first_tree_file)
+        second_head_and_relative_distance = self.get_head_and_relative_distance(second_tree_file)
+        first_relative_distance = first_head_and_relative_distance["relative_distance"]
+        second_relative_distance = second_head_and_relative_distance["relative_distance"]
+        first_tree_head = first_head_and_relative_distance["last_father"]
+        second_tree_head = second_head_and_relative_distance["last_father"]
+        if first_tree_head.contained_address != second_tree_head.contained_address:
+            if first_relative_distance > second_relative_distance:
+                first_tree_head.recursion_add_edge(second_tree_head.contained_address)
+                self.redis_session.srem('entry:addresses', second_tree_head.contained_address)
+                print(
+                    "first_tree_head " + str(first_tree_head.contained_address) + " points to " + str(second_tree_head.contained_address))
+            else:
+                second_tree_head.recursion_add_edge(first_tree_head.contained_address)
+                self.redis_session.srem('entry:addresses', first_tree_head.contained_address)
+                print(
+                    "second_tree_head " + str(second_tree_head.contained_address) + " points to " + str(first_tree_head.contained_address))
+
+    @staticmethod
+    def get_head_and_relative_distance(file_model: FileModel):
+        relative_distance = 0
+        file_father = file_model.get_call_in_files_models()
+        last_father = file_model
+        while file_father:
+            relative_distance += 1
+            last_father = file_father[0]
+            file_father = file_father[0].get_call_in_files_models()
+
+        return {"last_father": last_father, "relative_distance": relative_distance}
