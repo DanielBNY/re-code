6202fbc36d659c306a8ddd48225cf63a894dcf27
Daniel <daniel.benyaacov@gmail.com>
2021-01-31 23:54:53 +0200

    Renaming variables

--- HierarchyExtractor/FunctionsGraphExtractor.py
+++ HierarchyExtractor/FunctionsGraphExtractor.py
@@ -69,18 +69,19 @@ class FunctionsGraphExtractor:
             function_info = self.functions_info_collection.find({"_id": function_info_id}).next()
             if 'callrefs' in function_info:
                 for call_reference in function_info['callrefs']:
-                    called_function = self.get_valid_function_address(call_reference['addr'])
-                    if called_function and call_reference['type'] == 'CALL':
-                        source_function = function_info['offset']
-                        fnc_repo_actions = FunctionModel(address=str(source_function).encode(),
-                                                         redis_session=self.redis_session)
-                        if source_function != called_function:
-                            if ApiWrappers(self.redis_session).is_api_wrapper('function:' + str(called_function)):
-                                fnc_repo_actions.set_called_function_wrapper(called_function)
+                    called_function_address = self.get_valid_function_address(call_reference['addr'])
+                    if called_function_address and call_reference['type'] == 'CALL':
+                        source_function_address = function_info['offset']
+                        source_function_model = FunctionModel(address=str(source_function_address).encode(),
+                                                              redis_session=self.redis_session)
+                        if source_function_address != called_function_address:
+                            if ApiWrappers(self.redis_session).is_api_wrapper(
+                                    'function:' + str(called_function_address)):
+                                source_function_model.set_called_function_wrapper(called_function_address)
                             else:
-                                if self.redis_session.sismember("functions", f"function:{source_function}"):
-                                    if self.redis_session.sismember("functions", f"function:{called_function}"):
-                                        fnc_repo_actions.add_function_edge(str(called_function).encode())
+                                if self.redis_session.sismember("functions", f"function:{source_function_address}"):
+                                    if self.redis_session.sismember("functions", f"function:{called_function_address}"):
+                                        source_function_model.add_function_edge(str(called_function_address).encode())
 
     def set_entry_and_lonely_models(self):
         """
