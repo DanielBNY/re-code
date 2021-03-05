2ceea6680b6c7a2ae4f5847e8cc749a73deeb104
Daniel <daniel.benyaacov@gmail.com>
2021-03-05 14:06:02 +0200

    Rename the keys because the addresses are the functions addresses

--- HierarchyExtractor/Models.py
+++ HierarchyExtractor/Models.py
@@ -374,32 +374,32 @@ class SpecialModels:
 
 class LonelyModels(SpecialModels):
     def __init__(self, redis_session: redis.Redis):
-        SpecialModels.__init__(self, key_name='lonely:addresses', redis_session=redis_session)
+        SpecialModels.__init__(self, key_name='lonely:functions:addresses', redis_session=redis_session)
 
 
 class EntryModels(SpecialModels):
     def __init__(self, redis_session: redis.Redis):
-        SpecialModels.__init__(self, key_name='entry:addresses', redis_session=redis_session)
+        SpecialModels.__init__(self, key_name='entry:functions:addresses', redis_session=redis_session)
 
 
 class TreesEntriesFunctionsAddresses(SpecialModels):
     def __init__(self, redis_session: redis.Redis):
-        SpecialModels.__init__(self, key_name='trees_entries_functions:addresses', redis_session=redis_session)
+        SpecialModels.__init__(self, key_name='trees_entries:functions:addresses', redis_session=redis_session)
 
 
 class MultipleEntriesModels(SpecialModels):
     def __init__(self, redis_session: redis.Redis):
-        SpecialModels.__init__(self, key_name='multiple_entries:addresses', redis_session=redis_session)
+        SpecialModels.__init__(self, key_name='multiple_entries:functions:addresses', redis_session=redis_session)
 
 
 class RadareDetectedModels(SpecialModels):
     def __init__(self, redis_session: redis.Redis):
-        SpecialModels.__init__(self, key_name='radare_detected_models:addresses', redis_session=redis_session)
+        SpecialModels.__init__(self, key_name='radare_detected:functions:addresses', redis_session=redis_session)
 
 
 class RetdecDetectedModels(SpecialModels):
     def __init__(self, redis_session: redis.Redis):
-        SpecialModels.__init__(self, key_name='retdec_detected_models:addresses', redis_session=redis_session)
+        SpecialModels.__init__(self, key_name='retdec_detected:functions:addresses', redis_session=redis_session)
 
 
 def get_models_by_addresses(addresses, redis_session, model_name) -> List[NodeModel]:
