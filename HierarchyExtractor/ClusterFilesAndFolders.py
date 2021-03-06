2860ddfb4768e6bb500c7735fc0d568d6638a75a
Daniel <daniel.benyaacov@gmail.com>
2021-03-06 19:05:02 +0200

    Add type annotations for the functions in the ClusterFilesAndFolders

--- HierarchyExtractor/ClusterFilesAndFolders.py
+++ HierarchyExtractor/ClusterFilesAndFolders.py
@@ -1,9 +1,11 @@
-from Models import Files, TreesEntriesFunctionsAddresses, get_models_by_ids, TreeNodeModel
-from collections.abc import Sequence
+from Models import Files, TreesEntriesFunctionsAddresses, get_models_by_ids
+from Models import FileModel, FolderModel
+from typing import Union, List
+import redis
 
 
 class ClusterFilesAndFolders:
-    def __init__(self, redis_session, max_file_size, max_number_of_max_files_in_folder):
+    def __init__(self, redis_session: redis.Redis, max_file_size: int, max_number_of_max_files_in_folder: int):
         self.max_file_size = max_file_size
         self.max_number_of_max_files_in_folder = max_number_of_max_files_in_folder
         self.redis_session = redis_session
@@ -20,16 +22,16 @@ class ClusterFilesAndFolders:
         self.cluster_models(folders_entry_models,
                             max_node_size=self.max_file_size * self.max_number_of_max_files_in_folder)
 
-    def cluster_models(self, trees_entries_models, max_node_size):
+    def cluster_models(self, trees_entries_models: List[Union[FileModel, FolderModel]], max_node_size: int):
         for entry_model in trees_entries_models:
             self.cluster_sons_of_entry_point([entry_model], max_node_size)
 
-    def cluster_sons_of_entry_point(self, entry_node_model, max_node_size):
+    def cluster_sons_of_entry_point(self, entry_node_model: List[Union[FileModel, FolderModel]], max_node_size: int):
         neighbors_to_revisit = self.cluster_multiple_nodes(entry_node_model, max_node_size)
         while neighbors_to_revisit:
             neighbors_to_revisit = self.cluster_multiple_nodes(neighbors_to_revisit, max_node_size)
 
-    def cluster_multiple_nodes(self, nodes, max_node_size):
+    def cluster_multiple_nodes(self, nodes: List[Union[FileModel, FolderModel]], max_node_size: int):
         neighbors_to_revisit = []
         for node in nodes:
             returned_merged_nodes = self.get_merged_nodes(node, max_node_size)
@@ -37,12 +39,13 @@ class ClusterFilesAndFolders:
                 neighbors_to_revisit += returned_merged_nodes
         return neighbors_to_revisit
 
-    def get_merged_nodes(self, father_node: TreeNodeModel, max_node_size):
+    def get_merged_nodes(self, father_node: Union[FileModel, FolderModel], max_node_size: int) \
+            -> List[Union[FileModel, FolderModel]]:
         model_ids = father_node.get_call_out_models_ids()
         sons_models = get_models_by_ids(model_ids=model_ids, redis_session=self.redis_session)
         sum_of_sons = father_node.get_sum_of_sons()
         if not sons_models:
-            return
+            return []
         if sum_of_sons < max_node_size:
             if sum_of_sons > max_node_size / 2:
                 return self.merge_multiple_nodes(sons_models)
@@ -57,7 +60,9 @@ class ClusterFilesAndFolders:
             return self.divide_to_cluster(num_of_cluster, sons_models, max_node_size)
 
     @staticmethod
-    def divide_to_cluster(number_of_clusters, nodes_to_cluster, max_node_size):
+    def divide_to_cluster(number_of_clusters: int,
+                          nodes_to_cluster: List[Union[FileModel, FolderModel]],
+                          max_node_size: int) -> List[Union[FileModel, FolderModel]]:
         clustered_nodes = []
         smaller_node = None
         if len(nodes_to_cluster) >= number_of_clusters:
@@ -79,7 +84,8 @@ class ClusterFilesAndFolders:
             return nodes_to_cluster
 
     @staticmethod
-    def merge_multiple_nodes(nodes_to_cluster):
+    def merge_multiple_nodes(nodes_to_cluster: List[Union[FileModel, FolderModel]]) -> \
+            List[Union[FileModel, FolderModel]]:
         node_to_cluster_into = nodes_to_cluster[0]
         for node in nodes_to_cluster[1:]:
             node_to_cluster_into.recursion_cluster(node)
