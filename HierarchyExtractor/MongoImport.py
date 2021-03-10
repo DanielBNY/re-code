9ed9bc214697f611d1fb2f768097b65aff1ad675
Daniel <daniel.benyaacov@gmail.com>
2021-03-10 19:43:25 +0200

    Remove unsued function import_collection_from_json

--- HierarchyExtractor/MongoImport.py
+++ HierarchyExtractor/MongoImport.py
@@ -7,10 +7,3 @@ def import_collection_from_json_array(collection_name, file_path):
         f"mongoimport --db {conf.mongo_db['db_name']} --collection {collection_name} --file {file_path} --jsonArray")
     output = stream.read()
     return output
-
-
-def import_collection_from_json(collection_name, file_path):
-    stream = os.popen(
-        f"mongoimport --db {conf.mongo_db['db_name']} --collection {collection_name} --file {file_path}")
-    output = stream.read()
-    return output
