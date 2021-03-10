aa3dcf4491e97387d5995ec90cb85dcb91ecbffe
Daniel <daniel.benyaacov@gmail.com>
2021-03-10 18:45:24 +0200

    Move the input of the parameters from the flow function to the class init parameters

--- HierarchyExtractor/ExtractorsManager.py
+++ HierarchyExtractor/ExtractorsManager.py
@@ -13,7 +13,12 @@ import conf
 
 class ExtractorsManager:
     def __init__(self, recovered_project_path: str, redis_ip: str, mongo_ip: str, decompiler_path: str,
-                 mongo_db_port=27017):
+                 file_path_to_analyze: str, max_number_of_max_files_in_folder: int, max_file_size: int,
+                 number_of_processes: int, mongo_db_port=27017):
+        self.file_path_to_analyze = file_path_to_analyze
+        self.max_number_of_max_files_in_folder = max_number_of_max_files_in_folder
+        self.max_file_size = max_file_size
+        self.number_of_processes = number_of_processes
         self.redis_session = redis.Redis(redis_ip)
         self.mongo_client = MongoClient(mongo_ip, mongo_db_port)
         self.recovered_project_path = recovered_project_path
@@ -26,13 +31,13 @@ class ExtractorsManager:
         self.redis_session.flushdb()
         self.mongo_client.drop_database(conf.mongo_db["db_name"])
 
-    def flow(self, file_path_to_analyze, max_number_of_max_files_in_folder, max_file_size, number_of_processes):
+    def flow(self):
         self.cleanup()
-        bin_ex = BinaryExtractor(file_path_to_analyze, self.redis_session)
+        bin_ex = BinaryExtractor(self.file_path_to_analyze, self.redis_session)
         bin_ex.analyze_all_functions_calls()
         import_retdec_data = ImportRetdecData(redis_session=self.redis_session,
-                                              binary_extractor=bin_ex, analyzed_file=file_path_to_analyze,
-                                              number_of_processes=number_of_processes,
+                                              binary_extractor=bin_ex, analyzed_file=self.file_path_to_analyze,
+                                              number_of_processes=self.number_of_processes,
                                               decompiler_path=self.decompiler_path,
                                               decompiled_files_path=conf.retdec_decompiler["decompiled_file_path"])
         import_retdec_data.run()
@@ -40,7 +45,7 @@ class ExtractorsManager:
         FunctionsGraphExtractor(self.redis_session, self.mongo_client).run()
         CallTreeExtractor(self.redis_session).run()
         ClusterTrees(redis_session=self.redis_session).run()
-        ClusterFilesAndFolders(redis_session=self.redis_session, max_file_size=max_file_size,
-                               max_number_of_max_files_in_folder=max_number_of_max_files_in_folder).run()
+        ClusterFilesAndFolders(redis_session=self.redis_session, max_file_size=self.max_file_size,
+                               max_number_of_max_files_in_folder=self.max_number_of_max_files_in_folder).run()
         BuildSampleStructure(recovered_project_path=self.recovered_project_path.encode(),
                              redis_session=self.redis_session).run()
