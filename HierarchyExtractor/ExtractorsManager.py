e0a4305909642fb6e038c3eda4ab550a41552487
Daniel <daniel.benyaacov@gmail.com>
2021-03-10 18:59:14 +0200

    Add the functions info file path as a parameter

--- HierarchyExtractor/ExtractorsManager.py
+++ HierarchyExtractor/ExtractorsManager.py
@@ -14,7 +14,7 @@ import conf
 class ExtractorsManager:
     def __init__(self, recovered_project_path: str, redis_ip: str, mongo_ip: str, decompiler_path: str,
                  file_path_to_analyze: str, max_number_of_max_files_in_folder: int, max_file_size: int,
-                 number_of_processes: int, mongo_db_port=27017):
+                 number_of_processes: int, functions_info_file_path: str, mongo_db_port=27017):
         self.file_path_to_analyze = file_path_to_analyze
         self.max_number_of_max_files_in_folder = max_number_of_max_files_in_folder
         self.max_file_size = max_file_size
@@ -23,6 +23,7 @@ class ExtractorsManager:
         self.mongo_client = MongoClient(mongo_ip, mongo_db_port)
         self.recovered_project_path = recovered_project_path
         self.decompiler_path = decompiler_path
+        self.functions_info_file_path = functions_info_file_path
 
     def cleanup(self):
         if os.path.exists(self.recovered_project_path):
@@ -41,7 +42,7 @@ class ExtractorsManager:
                                               decompiler_path=self.decompiler_path,
                                               decompiled_files_path=conf.retdec_decompiler["decompiled_file_path"])
         import_retdec_data.run()
-        bin_ex.extract_functions_info('/tmp/analyzed', imported_collection_name="FunctionsInfo")
+        bin_ex.extract_functions_info(self.functions_info_file_path, imported_collection_name="FunctionsInfo")
         FunctionsGraphExtractor(self.redis_session, self.mongo_client).run()
         CallTreeExtractor(self.redis_session).run()
         ClusterTrees(redis_session=self.redis_session).run()
