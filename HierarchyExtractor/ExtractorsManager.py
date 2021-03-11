6e96337c0c301bd0d6e51b0392eecfa706bf16d5
Daniel <daniel.benyaacov@gmail.com>
2021-03-11 19:32:03 +0200

    Change the number of process parameter to be optional and the default value to be as the number of cores

--- HierarchyExtractor/ExtractorsManager.py
+++ HierarchyExtractor/ExtractorsManager.py
@@ -8,18 +8,22 @@ from BuildSampleStructure import BuildSampleStructure
 import shutil, os.path
 from ImportRetdecData import ImportRetdecData
 from ClusterTrees import ClusterTrees
+import multiprocessing
 
 
 class ExtractorsManager:
     def __init__(self, recovered_project_path: str, redis_ip: str, mongo_ip: str, decompiler_path: str,
                  file_path_to_analyze: str, max_number_of_max_files_in_folder: int, max_file_size: int,
-                 number_of_processes: int, functions_info_file_path: str, decompiled_files_path: str,
-                 functions_info_collection_name: str, mongo_db_name: str, mongo_db_port=27017):
+                 functions_info_file_path: str, decompiled_files_path: str, functions_info_collection_name: str,
+                 mongo_db_name: str, mongo_db_port=27017, number_of_processes=None):
         self.functions_info_collection_name = functions_info_collection_name
         self.file_path_to_analyze = file_path_to_analyze
         self.max_number_of_max_files_in_folder = max_number_of_max_files_in_folder
         self.max_file_size = max_file_size
-        self.number_of_processes = number_of_processes
+        if not number_of_processes:
+            self.number_of_processes = multiprocessing.cpu_count()
+        else:
+            self.number_of_processes = number_of_processes
         self.redis_session = redis.Redis(redis_ip)
         self.mongo_client = MongoClient(mongo_ip, mongo_db_port)
         self.recovered_project_path = recovered_project_path
