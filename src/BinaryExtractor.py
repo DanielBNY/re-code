4302b80f2a714e3cabe4a51a59e37826a27be76b
Daniel <daniel.benyaacov@gmail.com>
2021-04-23 18:33:47 +0300

    Add documentations for the BinaryExtractor class functions

--- src/BinaryExtractor.py
+++ src/BinaryExtractor.py
@@ -14,12 +14,20 @@ class BinaryExtractor:
         self.set_start_end_virtual_addresses()
 
     def set_start_end_virtual_addresses(self):
+        """
+        Sets the minimum and maximum virtual addresses in file.
+        """
         file_sections_json = json.loads(self.command_pipe.cmd('iSj'))
         self.start_virtual_address = self.get_start_virtual_address(file_sections_json)
         self.end_virtual_address = self.get_end_virtual_address(file_sections_json)
 
     @staticmethod
     def get_start_virtual_address(file_sections):
+        """
+        :param file_sections: list, list of file sections
+
+        Returns the minimum virtual file section address.
+        """
         min_address = file_sections[0]["vaddr"]
         for section in file_sections:
             if min_address > section["vaddr"]:
@@ -28,6 +36,11 @@ class BinaryExtractor:
 
     @staticmethod
     def get_end_virtual_address(file_sections):
+        """
+        :param file_sections: list, list of file sections
+
+        Returns the max virtual file section address.
+        """
         max_address = file_sections[0]["vaddr"] + file_sections[0]["vsize"]
         for section in file_sections:
             section_end_address = section["vaddr"] + section["vsize"]
@@ -36,6 +49,9 @@ class BinaryExtractor:
         return max_address
 
     def analyze_all_functions_calls(self):
+        """
+        Radare2 'aac' command analyze all functions calls
+        """
         self.command_pipe.cmd('aac')
 
     def finish_analysis(self):
@@ -43,6 +59,10 @@ class BinaryExtractor:
         self.command_pipe = None
 
     def export_functions_addresses(self):
+        """
+        Import all radare2 detected functions addresses into redis models.
+        Radare2 's @@ fcn.*' command goes over all detected functions and prints their addresses.
+        """
         functions_addresses = self.command_pipe.cmd(f"s @@ fcn.*")
         functions_addresses_list = functions_addresses.split('\n')
         for address in functions_addresses_list:
@@ -50,19 +70,45 @@ class BinaryExtractor:
                 RadareDetectedModels(redis_session=self.redis_session).add_address(int(address, 16))
 
     def extract_functions_info(self, output_path: str, imported_collection_name: str, mongo_db_name: str):
+        """
+        :param output_path:
+        :param imported_collection_name:
+        :param mongo_db_name:
+
+        Export the functions info into a json file and then import it into mongodb for fast function info read time.
+        """
         self.export_functions_info(output_json_path=output_path)
         self.import_functions_info(input_json_path=output_path, imported_collection_name=imported_collection_name,
                                    mongo_db_name=mongo_db_name)
 
     def export_functions_info(self, output_json_path):
+        """
+        :param output_json_path: str, json file path
+
+        Radare2 'aflj' command list the functions info in a json format.
+        The command output is written into the output json path.
+        """
         self.command_pipe.cmd(f"aflj > {output_json_path}")
 
     @staticmethod
     def import_functions_info(mongo_db_name: str, input_json_path: str, imported_collection_name: str):
+        """
+        :param mongo_db_name: str, mongodb name.
+        :param input_json_path: str, json array file path.
+        :param imported_collection_name: str, collection name to import the json array.
+
+        Import a json array file with the mongoimport cli tool to a mongo DB collection.
+        """
         stream = os.popen(
             f"mongoimport --db {mongo_db_name} --collection {imported_collection_name} --file {input_json_path} --jsonArray")
         output = stream.read()
         return output
 
     def analyze_function_at_address(self, address):
+        """
+        :param address: int, address inside the binary file.
+
+        Radare2 's address' command jump to the address in the binary.
+        Radare2 'af' analyze the function in the current address.
+        """
         self.command_pipe.cmd(f"s {address}; af")
