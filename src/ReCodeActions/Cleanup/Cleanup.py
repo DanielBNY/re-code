f7c90085583d531acff80819311b5d5d0a798d4f
Daniel <daniel.benyaacov@gmail.com>
2021-05-07 13:50:39 +0300

    Create the remove_and_recreate function

--- src/ReCodeActions/Cleanup/Cleanup.py
+++ src/ReCodeActions/Cleanup/Cleanup.py
@@ -1,7 +1,7 @@
 from src.AbstractClasses import Action
 from pymongo import MongoClient
 from os.path import exists
-import os.path
+from os import mkdir
 import shutil
 import redis
 
@@ -9,9 +9,7 @@ import redis
 class Cleanup(Action):
     def __init__(self, redis_session: redis.Redis, mongo_client: MongoClient, file_path_to_analyze: str,
                  functions_info_file_path: str, temporary_sample_data_directory: str, mongo_db_name: str,
-                 decompiled_files_path: str, recovered_project_path: str,
-                 decompiler_path: str):
-
+                 decompiled_files_path: str, recovered_project_path: str, decompiler_path: str):
         self.redis_session = redis_session
         self.mongo_client = mongo_client
         self.file_path_to_analyze = file_path_to_analyze
@@ -23,24 +21,14 @@ class Cleanup(Action):
         self.mongo_db_name = mongo_db_name
 
     def run(self):
-        self.re_create_recovered_project_path()
-        self.re_create_temporary_sample_directories()
-        self.decompiled_files_cleanup()
+        self.remove_and_recreate(self.recovered_project_path)
+        self.remove_and_recreate(self.temporary_sample_data_directory)
+        self.remove_and_recreate(self.decompiled_files_path)
         self.redis_session.flushdb()
         self.mongo_client.drop_database(self.mongo_db_name)
 
-    def re_create_recovered_project_path(self):
-        if os.path.exists(self.recovered_project_path):
-            shutil.rmtree(self.recovered_project_path)
-        os.mkdir(self.recovered_project_path)
-
-    def re_create_temporary_sample_directories(self):
-        if os.path.exists(self.temporary_sample_data_directory):
-            shutil.rmtree(self.temporary_sample_data_directory)
-        os.mkdir(self.temporary_sample_data_directory)
-        os.mkdir(self.decompiled_files_path)
-
-    def decompiled_files_cleanup(self):
-        if exists(self.decompiled_files_path):
-            shutil.rmtree(self.decompiled_files_path)
-        os.mkdir(self.decompiled_files_path)
+    @staticmethod
+    def remove_and_recreate(path):
+        if exists(path):
+            shutil.rmtree(path)
+        mkdir(path)
